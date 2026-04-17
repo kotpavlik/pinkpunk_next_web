bac0463e2e56e1fd6e169e7c6865e463402cd873
kotpavlik <kotpavlik88@gmail.com>
2026-04-17 12:58:01 +0300

    gmail for alfabank

--- src/components/ui/footer/InfoBlocks.tsx
+++ src/components/ui/footer/InfoBlocks.tsx
@@ -125,7 +125,7 @@ const InfoBlocks = memo(function InfoBlocks() {
                             <a href="mailto:pinkpunk.company@gmail.com" className="flex items-center gap-2 ">
                                 <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
                                     <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
-                                </svg> Email
+                                </svg> pinkpunk.company@gmail.com
                             </a>
                         </p>
                         <p className='text-sm font-cabinet-grotesk lowercase text-gray-500 mt-4'>
